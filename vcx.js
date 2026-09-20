db89cd0485864b948b2a26914d870974cc89135b
akunpribrobi-bit <akunpribrobi@gmail.com>
2026-09-20 22:05:18 +0700

    Update console log message and add loop

--- vcx.js
+++ vcx.js
@@ -1,4 +1,11 @@
 console.log("=================================");
-console.log("Halo! Ini teks dari repo GitHub.");
+console.log("Halo !.");
 console.log("Waktu server: " + new Date().toLocaleDateString());
 console.log("=================================");
+
+const teks = "spaming flow js! ";
+
+// Melakukan loop sebanyak 5 kali
+for (let i = 0; i < 1000; i++) {
+  console.log(teks);
+}
